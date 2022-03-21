c653c8dfcd6d377b4b0d14b422975042007328e4
e-ikasus <contact@e-ikasus.fr>
2022-03-21 12:56:00 +0100

    modification message hello world

--- src/main/java/fr/eni/lesobjetssontnosamis/HelloServlet.java
+++ src/main/java/fr/eni/lesobjetssontnosamis/HelloServlet.java
@@ -11,7 +11,7 @@ public class HelloServlet extends HttpServlet
 
 	public void init()
 	{
-		message = "Hello World!";
+		message = "Salut tout le monde.";
 	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
